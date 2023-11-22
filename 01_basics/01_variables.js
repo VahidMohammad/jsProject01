6e095ed1b461bf2f174727e92b5a938db9f130aa
Vahid Moammad <148212533+VahidMohammad@users.noreply.github.com>
2023-11-22 04:54:41 +0000

    configure commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+variable.js  
+
+const acountId = 77777
+let acountEmail = "riaAANKHA@s.com"
+var acountPassword = "123456"
+acountCity = "Shivpuri"
+let acountState
+
+// acountId = 2 acount id not chngeble bcz "const" not change   
+/* var not usable for latest projects
+becauese of issue in block scope and fubtional scope
+*/
+
+acountEmail = "same@sam.com"
+acountPassword = "0987654"
+acountCity = "gwalior"
+ 
+console.log(acountId);
+
+console.table([acountId, acountEmail, acountPassword, acountCity, acountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+DAta_types.js 
+
+
+Number = 12345
+BigInt
+String =""
+Boolean = true/false
+// null = standaloneValue  
+undefined = 
+Symbol = Unique
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
